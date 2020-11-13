71fa15d22d705a63997bb0439677ed80e9f14677
Gaimo <gaimo.tkx@gmail.com>
2020-11-13 17:00:11 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,15 +2,49 @@ local discordia = require('discordia')
 local client = discordia.Client()
  
 local prefix = "!"
- 
+local answers = {
+
+    -- Comando = "Resposta"
+
+    ping = "pong",
+    marco = "polo",
+    comando = "resposta",
+    nome = "Joao",
+
+
+    ["comando algo"] = "test",
+
+
+
+
+}
+
 client:on('ready', function()
     print('Logged in as '.. client.user.username)
 end)
  
 client:on('messageCreate', function(message)
-    if string.lower(message.content) == prefix..'ping' then
-        message.channel:send('Pong!')
+
+
+    -- ping
+
+    for command, answer in pairs(answers) do
+
+        if string.lower(message.content) == prefix..command then
+            message.channel:send(answer)
+            break
+        end
+
     end
+
+    --
+
+    
+    
+
+
+
+
 end)
  
 client:run('Bot '..io.open("./token.txt"):read())
